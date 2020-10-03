c4e00f89d0512fce69f39eb527b69a8fe49dbb50
Harmony Lab <lead@harmonylab.com>
2020-10-02 20:34:42 -0700

    Update

--- ClaverMessageBoard.py
+++ ClaverMessageBoard.py
@@ -1,11 +1,13 @@
 import sys
+
 import gi
+
 gi.require_version('Gtk', '3.0')
 
 from gi.repository import Gtk, Gdk
 from .interface.settings.Settings import res_dir
 from .interface.gui.GuiManager import GuiManager
-from interface.network.NodeClient import NodeClient
+
 
 class ClaverMessageBoard(Gtk.Application):
 
